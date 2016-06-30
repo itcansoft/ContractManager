5ed3ef8aac386d644e3fc07882817fbdcb5741ab
Mohamed Maza <Mohamed.Maza.Extern@zalando.de>
2016-06-30 11:43:18 +0200

    Initial commit

---
+++ src/main/java/ContractManager.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mmaza on 6/30/16.
+ */
+public class ContractManager {
+
+    public static void main(final String[] args) {
+        System.out.println("You entered: " + args.length + " paramns.");
+    }
+}
